b487c94e0edc53d7598f65c85c17dbf4f61ec347
hitsan <15902694+hitsan@users.noreply.github.com>
2026-06-01 22:26:19 +0900

    add 3 col

---
+++ template/layouts/cards.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="slidev-layout cards">
+    <div class="cards-header">
+      <slot />
+    </div>
+    <div class="cards-grid">
+      <div class="card">
+        <slot name="card1" />
+      </div>
+      <div class="card">
+        <slot name="card2" />
+      </div>
+      <div class="card">
+        <slot name="card3" />
+      </div>
+    </div>
+  </div>
+</template>
