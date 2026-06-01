056cd0fbd5f50e987a917765d03e3453fcb7a945
hitsan <15902694+hitsan@users.noreply.github.com>
2026-06-02 01:38:12 +0900

    fix width

---
+++ template/layouts/compare.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="slidev-layout compare">
+    <div class="compare-header">
+      <slot />
+    </div>
+    <div class="compare-cols">
+      <div class="compare-col">
+        <slot name="col1" />
+      </div>
+      <div class="compare-col">
+        <slot name="col2" />
+      </div>
+    </div>
+    <div v-if="$slots['bottom']" class="compare-bottom">
+      <slot name="bottom" />
+    </div>
+  </div>
+</template>
