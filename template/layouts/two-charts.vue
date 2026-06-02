74c8feda0e47606f7d4b919db1ab69918c63e7fe
hitsan <15902694+hitsan@users.noreply.github.com>
2026-06-02 21:29:03 +0900

    add layout

---
+++ template/layouts/two-charts.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="slidev-layout two-charts">
+    <div class="two-charts-header">
+      <slot />
+    </div>
+    <div class="two-charts-body">
+      <div class="two-charts-item">
+        <slot name="chart1" />
+      </div>
+      <div class="two-charts-item">
+        <slot name="chart2" />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ template/layouts/two-images.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="slidev-layout two-images">
+    <div class="two-images-header">
+      <slot />
+    </div>
+    <div class="two-images-body">
+      <div class="two-images-item">
+        <slot name="image1" />
+      </div>
+      <div class="two-images-item">
+        <slot name="image2" />
+      </div>
+    </div>
+  </div>
+</template>
